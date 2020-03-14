257c65727fbe51e0f534d6f436ff3521c5ad3178
vysdenis <vysdenis@gmail.com>
2020-03-14 20:48:20 +0500

    Modified code: 8 attempts

--- module_0/project_0.py
+++ module_0/project_0.py
@@ -2,37 +2,44 @@
 """
 Created on Sat Mar 14 18:22:48 2020
 
-Original code. Result: 30 attempts
+Modified code. Result: 8 attempts
 
 @author: vysdenis
 """
 
 import numpy as np
        
-def score_game(game_core_v2):
+def score_game(game_core_v3):
     '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
     count_ls = []
     np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
     random_array = np.random.randint(1, 101, size=(1000))
     for number in random_array:
-        count_ls.append(game_core_v2(number))
+        count_ls.append(game_core_v3(number))
     score = int(np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
     return(score)
 
 
-def game_core_v2(number):
-    '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно или меньше нужного.
+def game_core_v3(number):
+    '''Сначала устанавливаем любое число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно или меньше нужного.
        Функция принимает загаданное число и возвращает число попыток'''
     count = 0
-    predict = np.random.randint(1,100)
+    predict = 50
+    step = 0
     while number != predict:
         count+=1
+        if count == 1:
+            step = 20
+        elif count == 2:
+            step = 10
+        else:
+            step = 1
         if number > predict: 
-            predict += 1
+            predict += step
         elif number < predict: 
-            predict -= 1
+            predict -= step
     return(count) # выход из цикла, если угадали
 
 # Проверяем
-score_game(game_core_v2)
+score_game(game_core_v3)
